0f497d99b16a950fc94c0a37bbe2c03377dc8728
pr <pr@fivem.net>
2017-06-05 14:58:09 +0200

    initial: commit one resource

---
+++ resources/temp/__resource.lua
@@ -0,0 +1,10 @@
+resource 'temp'
+
+author 'radicalium <https://github.com/radicalium>'
+maintainer 'radicalium <https://github.com/radicalium>'
+
+version '0.1.0'
+
+resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
+
+client_script 'hey.lua'
